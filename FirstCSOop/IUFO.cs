9bbe0da321ea246c1b4e19d08e147589faffbeb5
Sara Betancourth <sarabet93@gmail.com>
2022-11-20 11:28:38 -0500

    added interface

---
+++ FirstCSOop/IUFO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSOop
+{
+    internal interface IUFO
+    {
+        void fly();
+        void goToHyperspace();
+        void land();
+    }
+}
