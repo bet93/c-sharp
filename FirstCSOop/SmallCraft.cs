1da47a7abfccbd2cd8366b93c55f111f0a496a1a
Sara Betancourth <sarabet93@gmail.com>
2022-11-20 11:41:07 -0500

    update to small craft class

--- FirstCSOop/SmallCraft.cs
+++ FirstCSOop/SmallCraft.cs
@@ -9,11 +9,6 @@ namespace FirstCSOop
     // class implements interface IUFO
     internal class SmallCraft : IUFO
     {
-        public SmallCraft(string name)
-        {
-            this.name = name;
-            Console.WriteLine("Hi, I am a small craft and my name is " + name);
-        }
         public void fly()
         {
             Console.WriteLine("Small craft can't fly around the world.");
