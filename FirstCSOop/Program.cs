ed3644fc1b2094f49396faddcf498dbb08e31e1c
Sara Betancourth <sarabet93@gmail.com>
2022-11-20 11:21:33 -0500

    instatiate instances of dog, cat, and bird classes in main and call each method

--- FirstCSOop/Program.cs
+++ FirstCSOop/Program.cs
@@ -2,5 +2,41 @@
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            // part 1
+
+            // instantiate an object of Dog
+            Dog dog = new Dog("Charlie");
+            dog.move();
+            dog.sleep();
+            dog.eat();
+            dog.sing();
+            dog.wash();
+            dog.turnAround();
+
+
+            // instantiate an object of Cat
+            Cat cat = new Cat("Kovu");
+            cat.move();
+            cat.sleep();
+            cat.eat();
+            cat.sing();
+            cat.wash();
+            cat.turnAround();
+
+            // instantiate an object of Bird
+            Bird bird = new Bird("Blu");
+            bird.move();
+            bird.sleep();
+            bird.eat();
+            bird.sing();
+            bird.wash();
+            bird.turnAround();
+
+
+            // part 2
+        }
+
     }
 }
