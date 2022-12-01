ed4878c18b2e6d74dae0077bd7f980b7b1f6de3a
Sara Betancourth <sarabet93@gmail.com>
2022-11-30 20:12:56 -0500

    update to problem1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -2,12 +2,18 @@
 {
   class Problem1
   {
+    // takes a sequence of integers and returns a sequence of integers
+    // first removes all multiples of 5 greater than 50
+    // then cubes each number
+    // finally, removes any resulting integer that is even
     public static IEnumerable<int> myFilter(IEnumerable<int> input)
     {
       IEnumerable<int> result;
-      result = input.Where(x => x % 5 == 0).Where(x => x < 50).Select(x => x * x * x).Where(x => x % 2 != 0);
+      
+      result = input.Where(x => x % 5 != 0 || x <= 50).Select(x => x * x * x).Where(x => x % 2 != 0);
 
       return result;
+
     }
   }
 
@@ -15,9 +21,10 @@
   {
     static void Main(string[] args)
     {
-      Random rnd = new Random(5);
-      var listForProblem = Enumerable.Range(1, 100).Select(i => rnd.Next() % 101);
-      var answer = Problem1.myFilter(listForProblem);
+      Random rnd = new Random(5); // important to seed with 5 for repeatability.
+      var listForProblem = Enumerable.Range(1, 100).Select(i => rnd.Next() % 101); // range 0 to 100
+      var answer = Problem1.myFilter(listForProblem); // answer is an IEnumerable<int>
+
       foreach (int i in answer)
       {
         Console.WriteLine(i);
