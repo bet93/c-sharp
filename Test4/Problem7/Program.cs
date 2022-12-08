abf3711954b238abb80ad4bef46a053cf2f31f5f
Sara Betancourth <sarabet93@gmail.com>
2022-12-08 18:22:01 -0500

    problem 7 completed

---
+++ Test4/Problem7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Problem7
+{
+  class Problem7
+  {
+    public static IEnumerable<int> linqPrimes(IEnumerable<int> input2)
+    {
+      IEnumerable<int> result;
+      result = input2.Where(x => isPrime(x));
+      return result;
+    }
+
+    public static bool isPrime(int number)
+    {
+      // if (number <= 1) return false;
+      if (number == 2) return true;
+      if (number % 2 == 0) return false;
+
+      var limit = (int)Math.Floor(Math.Sqrt(number));
+
+      for (int i = 3; i <= limit; i += 2)
+        if (number % i == 0)
+          return false;
+
+      return true;
+    }
+
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numbers2 = { 30, 31, 40, 41, 50, 51, 60, 61, 70, 71, 72, 74, 75, 100, 101, 101, 200 };
+      List<int> list2 = numbers2.OfType<int>().ToList();
+
+      var answer = Problem7.linqPrimes(list2);
+      foreach (int i in answer)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
